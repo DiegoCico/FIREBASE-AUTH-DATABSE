1494336039d0bdc302e38c8a380bb2d8ebe9062b
Diego Cicotoste <cicotosted@gmail.com>
2024-01-20 23:25:25 +0000

    saveEvents

---
+++ Visitor-Manager/EventsListViewController.swift
@@ -0,0 +1,34 @@
+//
+//  EventsListViewController.swift
+//  Visitor-Manager
+//
+//  Created by Diego Cicotoste on 1/21/24.
+//
+
+import UIKit
+import Firebase
+
+class EventsListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    @IBAction func addClicked(_ sender: Any) {
+    }
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
