ded4c6e7c3b90ebaa9572a6db73366656efee1f4
Diego Cicotoste <cicotosted@gmail.com>
2024-01-23 11:44:57 +0000

    fix

--- Visitor-Manager/PopupViewController.swift
+++ Visitor-Manager/PopupViewController.swift
@@ -24,36 +24,24 @@ class PopupViewController: UIViewController {
         let title = titleTextField.text ?? ""
         let description = descriptionTextField.text ?? ""
         let date = datePicker.date
-        let ref = Database.database().reference()
-
-        //FIRE DATABSE
-         // Process the data (e.g., save to Firebase, delegate it back to main screen, etc.)
-         let data: [String: Any] = ["title": title, "description": description, "date": date.timeIntervalSince1970] // using UNIX timestamp for date
-
-         // You can save the data under a specific path like "userItems"
-         ref.child("userItems").childByAutoId().setValue(data) { error, _ in
-             if let error = error {
-                 print("Data could not be saved: \(error.localizedDescription)")
-             } else {
-                 print("Data saved successfully")
-             }
-         }
         
-        // FOR FIRESTORE
-//        let db = Firestore.firestore()
-//        let data: [String: Any] = ["title": title, "description": description, "date": Timestamp(date: date)] // Firestore Timestamp for date
-//
-//        // Add a new document in collection "userItems"
-//        db.collection("userItems").addDocument(data: data) { error in
-//            if let error = error {
-//                print("Error adding document: \(error.localizedDescription)")
-//            } else {
-//                print("Document added successfully")
-//            }
-//        }
+        guard let userID = Auth.auth().currentUser?.uid else {
+               print("User not logged in")
+               return
+           }
 
+           let data: [String: Any] = ["title": title, "description": description, "date": date.timeIntervalSince1970]
 
-         dismiss(animated: true, completion: nil)
+           let ref = Database.database().reference()
+           ref.child("userItems").child(userID).childByAutoId().setValue(data) { error, _ in
+               if let error = error {
+                   print("Data could not be saved: \(error.localizedDescription)")
+               } else {
+                   print("Data saved successfully")
+               }
+           }
     }
-    
-}
+    }
+
+
+
