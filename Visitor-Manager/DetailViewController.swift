7d9fb2d5e77df4ee31980e10cdcc8617cd469d43
Diego Cicotoste <cicotosted@gmail.com>
2024-01-28 15:20:03 +0000

    fix in firebase rules

--- Visitor-Manager/DetailViewController.swift
+++ Visitor-Manager/DetailViewController.swift
@@ -1,163 +1,78 @@
 import UIKit
-import FirebaseDatabase
 import Firebase
+import FirebaseAuth
+import FirebaseDatabase
 
-class DetailViewController: UIViewController, UITableViewDelegate {
+class DetailViewController: UIViewController {
+
     var event: EventsListViewController.Event?
     var dateFormatter: DateFormatter?
-    
+
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var dateLabel: UILabel!
     @IBOutlet weak var descriptionLabel: UILabel!
-    @IBOutlet weak var tableView: UITableView! // Connect this IBOutlet to your table view in the storyboard
-    
-    struct User {
-        var name: String
-        var email: String
-    }
-    
-    var users: [User] = [] // Data source for the table view
-    
-    var isAddUserAlertShown = false // Flag to track if the alert has been shown
-    
-    // Reference to the Firebase database
-    let databaseRef = Database.database().reference()
-    
-    // Variable to store the current user's UID
-    var currentUserUID: String?
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "UserCell")
-        
-        if let event = self.event, let dateFormatter = self.dateFormatter {
-            // Display event information in the UI elements
-            titleLabel.text = event.title
-            descriptionLabel.text = event.description
-            dateLabel.text = dateFormatter.string(from: event.date)
-            
-            // Set the title of the page to the event title
-            self.title = event.title
+
+        if let selectedEvent = event {
+            titleLabel.text = selectedEvent.title
+            dateLabel.text = dateFormatter?.string(from: selectedEvent.date)
+            descriptionLabel.text = selectedEvent.description
         }
-        
-        // Set the table view delegate and data source
-        tableView.delegate = self
-        tableView.dataSource = self
-        
-        // Allow editing (swipe-to-delete) for the table view
-        tableView.isEditing = true
-        
-        // Get the current user's UID
-        if let currentUser = Auth.auth().currentUser {
-            currentUserUID = currentUser.uid
+    }
+
+    @IBAction func addButtonClicked(_ sender: Any) {
+        // Create a UIAlertController
+        let alertController = UIAlertController(title: "Add Event",
+                                                message: "Enter name and email",
+                                                preferredStyle: .alert)
+
+        // Add text fields for name and email
+        alertController.addTextField { (textField) in
+            textField.placeholder = "Name"
         }
-        
-        // Load user data from Firebase and populate the users array
-        if let currentUserUID = currentUserUID {
-            let userNodeRef = databaseRef.child("userItems").child(currentUserUID)
-            userNodeRef.observe(.childAdded) { (snapshot) in
-                if let userData = snapshot.value as? [String: String],
-                   let name = userData["name"],
-                   let email = userData["email"] {
-                    let newUser = User(name: name, email: email)
-                    self.users.append(newUser)
-                    self.tableView.reloadData()
+
+        alertController.addTextField { (textField) in
+            textField.placeholder = "Email"
+        }
+
+        // Add "Save" action
+        let saveAction = UIAlertAction(title: "Save", style: .default) { (_) in
+            if let nameTextField = alertController.textFields?[0],
+               let emailTextField = alertController.textFields?[1],
+               let name = nameTextField.text,
+               let email = emailTextField.text,
+               let currentUser = Auth.auth().currentUser {
+
+                // Create a dictionary with name and email
+                let userData: [String: Any] = [
+                    "name": name,
+                    "email": email
+                ]
+
+                // Reference to the user's account data
+                let userRef = Database.database().reference().child("userItems").child(currentUser.uid)
+
+                // Save the data in Firebase
+                userRef.setValue(userData) { (error, _) in
+                    if let error = error {
+                        print("Data could not be saved: \(error.localizedDescription)")
+                    } else {
+                        print("Data saved successfully")
+                    }
                 }
             }
         }
-    }
-    
-    // Function to add a user when the "Add User" button is tapped
-    @IBAction func addUserButtonTapped(_ sender: UIButton) {
-        if isAddUserAlertShown {
-            isAddUserAlertShown = false
-            return // Exit early if the alert has already been shown
-        }
-        
-        let alertController = UIAlertController(title: "Add User", message: "Enter Name and Email", preferredStyle: .alert)
-        
-        // Add text fields for name and email input
-        alertController.addTextField { (nameTextField) in
-            nameTextField.placeholder = "Name"
-        }
-        
-        alertController.addTextField { (emailTextField) in
-            emailTextField.placeholder = "Email"
-        }
-        
-        let addAction = UIAlertAction(title: "Add", style: .default) { (_) in
-            if let nameTextField = alertController.textFields?.first,
-               let emailTextField = alertController.textFields?.last,
-               let name = nameTextField.text,
-               let email = emailTextField.text,
-               !name.isEmpty,
-               !email.isEmpty {
-                
-                let newUser = User(name: name, email: email)
-                self.addUser(user: newUser)
-                
-                self.isAddUserAlertShown = true // Set the flag to true after showing the alert
-            }
-        }
-        
-        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
-        
-        alertController.addAction(addAction)
-        alertController.addAction(cancelAction)
-        
-        present(alertController, animated: true, completion: nil)
-    }
-    
-    // Add functions to manage users here
-    func addUser(user: User) {
-        // Add the user to the local data source (users array)
-        users.append(user)
-        
-        // Save the user to Firebase Realtime Database under the current user's UID
-        if let currentUserUID = currentUserUID {
-            let userRef = databaseRef.child("userItems").child(currentUserUID).childByAutoId()
-            userRef.setValue(["name": user.name, "email": user.email])
-        }
-        
-        // Reload the table view to reflect the changes
-        tableView.reloadData()
-    }
-    
-    func sendEmailInvitation(to user: User) {
-        // Implement email invitation logic (e.g., using Firebase Authentication)
-    }
-}
 
-// Implement UITableViewDataSource methods
-extension DetailViewController: UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return users.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "UserCell", for: indexPath)
-        let user = users[indexPath.row]
-        cell.textLabel?.text = user.name
-        cell.detailTextLabel?.text = user.email
-        return cell
-    }
-    
-    // Implement the swipe-to-delete functionality
-    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Remove the user from the data source (users array)
-            users.remove(at: indexPath.row)
-            
-            // Update the table view to reflect the deletion
-            tableView.deleteRows(at: [indexPath], with: .fade)
-            
-            // Remove the user from the current user's node in Firebase Realtime Database
-            if let currentUserUID = currentUserUID, indexPath.row < users.count {
-                let deletedUser = users[indexPath.row]
-                let userRef = databaseRef.child("userItems").child(currentUserUID).child(deletedUser.name) // Assuming name is a unique identifier for users
-                userRef.removeValue()
-            }
-        }
+        // Add "Cancel" action
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
+
+        // Add actions to the UIAlertController
+        alertController.addAction(saveAction)
+        alertController.addAction(cancelAction)
+
+        // Present the UIAlertController
+        self.present(alertController, animated: true, completion: nil)
     }
 }
--- Visitor-Manager/EventsListViewController.swift
+++ Visitor-Manager/EventsListViewController.swift
@@ -3,83 +3,85 @@ import Firebase
 import FirebaseAuth
 import FirebaseDatabase
 
-// Define EventsListViewController class, inheriting from UIViewController and conforming to UITableViewDataSource
 class EventsListViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
-    // Array to store event data
     var events: [Event] = []
-    
-    // Outlet for the table view
     @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Set the table view's data source to this view controller
         tableView.dataSource = self
         tableView.delegate = self
-        // Fetch events from Firebase
         fetchEventsFromFirebase()
-        
-        // Register a UITableViewCell for reuse
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: "EventCell")
-        
     }
     
-    // Define Event struct to model event data
     struct Event {
         let title: String
         let description: String
         let date: Date
+        let userID: String // Include the user's UID
     }
     
     lazy var dateFormatter: DateFormatter = {
-         let formatter = DateFormatter()
-         formatter.dateStyle = .medium
-         formatter.timeStyle = .none
-         return formatter
-     }()
+        let formatter = DateFormatter()
+        formatter.dateStyle = .medium
+        formatter.timeStyle = .none
+        return formatter
+    }()
+    
+    func saveEventToFirebase(event: Event) {
+        guard let userID = Auth.auth().currentUser?.uid else {
+            print("No user logged in")
+            return
+        }
+        
+        let eventRef = Database.database().reference().child("events").childByAutoId()
+        let eventData: [String: Any] = [
+            "title": event.title,
+            "description": event.description,
+            "date": event.date.timeIntervalSince1970,
+            "userID": userID  // Include the user's UID
+        ]
+        
+        eventRef.setValue(eventData) { (error, _) in
+            if let error = error {
+                print("Data could not be saved: \(error.localizedDescription)")
+            } else {
+                print("Data saved successfully")
+            }
+        }
+    }
     
     // Function to fetch events from Firebase
     func fetchEventsFromFirebase() {
-        // Guard to check if a user is logged in
         guard let userID = Auth.auth().currentUser?.uid else {
-               print("No user logged in")
-               // Handle the case where no user is logged in
-               return
+            print("No user logged in")
+            return
         }
         
-        // Reference to the Firebase database node
         let ref = Database.database().reference().child("userItems").child(userID)
-        // Observe changes in the Firebase database
-        ref.observe(.value) { snapshot in
+        ref.observe(.childAdded) { snapshot  in
             print("Snapshot: \(snapshot)")
-            var newEvents: [Event] = []
-            // Iterate through children of the snapshot
-            for child in snapshot.children {
-                if let snapshot = child as? DataSnapshot,
-                   let value = snapshot.value as? [String: Any],
-                   let title = value["title"] as? String,
-                   let description = value["description"] as? String,
-                   let timestamp = value["date"] as? TimeInterval {
-                    let date = Date(timeIntervalSince1970: timestamp)
-                    // Append new events to the array
-                    newEvents.append(Event(title: title, description: description, date: date))
+            if let value = snapshot.value as? [String: Any],
+               let title = value["title"] as? String,
+               let description = value["description"] as? String,
+               let timestamp = value["date"] as? TimeInterval {
+                let date = Date(timeIntervalSince1970: timestamp)
+                let newEvent = Event(title: title, description: description, date: date, userID: userID)
+                self.events.append(newEvent)
+                DispatchQueue.main.async {
+                    self.tableView.reloadData()
                 }
-            }
-            // Reload table view on the main thread
-            DispatchQueue.main.async {
-                self.events = newEvents
-                self.tableView.reloadData()
             }
         }
     }
+
     
-    // UITableViewDataSource method to return the number of rows
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-           return events.count
+        return events.count
     }
 
-    // UITableViewDataSource method to configure each cell
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "EventCell", for: indexPath)
         let event = events[indexPath.row]
@@ -90,22 +92,19 @@ class EventsListViewController: UIViewController, UITableViewDataSource, UITable
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         self.performSegue(withIdentifier: "showDetailSegue", sender: self)
-        }
+    }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-           if segue.identifier == "showDetailSegue" {
-               if let detailVC = segue.destination as? DetailViewController,
-                  let indexPath = tableView.indexPathForSelectedRow {
-                   let selectedEvent = events[indexPath.row]
-                   detailVC.event = selectedEvent
-               }
-           }
-       }
+        if segue.identifier == "showDetailSegue" {
+            if let detailVC = segue.destination as? DetailViewController,
+               let indexPath = tableView.indexPathForSelectedRow {
+                let selectedEvent = events[indexPath.row]
+                detailVC.event = selectedEvent
+            }
+        }
+    }
 
-    // Action method for Add button
     @IBAction func addClicked(_ sender: Any) {
         // Implement the action for your Add button here.
     }
-
-
 }
